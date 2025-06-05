87d2fd7be9be29c50149e9e8d1bab4916a4a32c2
Reddyscripts <c08121223324@gmail.com>
2025-06-04 19:17:00 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';  // include .jsx extension or omit, both usually work
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
